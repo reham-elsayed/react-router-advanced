58052d94b552f9441468e6490ad457e2996749fa
reham-elsayed <rehamshipl445@gmail.com>
2025-03-06 20:47:58 +0200

    movie picker initial project setup

---
+++ react-movie-picker-initial-setup/src/MovieGenres/MovieGenres.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const MovieGenres = () => {
+  return (
+    <div>
+      MovieGenres
+   
+    </div>
+  );
+};
+
+export default MovieGenres;
---
+++ react-movie-picker-initial-setup/src/Nav/Nav.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+const Nav = () => {
+  return (
+    <nav className="flex justify-between items-center bg-yellow-200">
+      <div>Movies</div>
+      <div>
+        <ul className="flex justify-center p-3">
+        
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
